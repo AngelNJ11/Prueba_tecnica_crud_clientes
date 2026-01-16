748cbdf2320fb8b4f400f09d609baac191f6033e
AngelNJ11 <angelnj733@gmail.com>
2026-01-16 01:48:29 -0500

    Borrando import no usados

--- src/main/java/com/ang/nav/controller/ClienteController.java
+++ src/main/java/com/ang/nav/controller/ClienteController.java
@@ -7,7 +7,6 @@ import com.ang.nav.services.ICliente;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
