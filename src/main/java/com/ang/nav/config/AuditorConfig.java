2f6e991351a347b7026bd46679d19e99676d75ca
AngelNJ11 <angelnj733@gmail.com>
2026-01-15 19:34:47 -0500

    Implementación de documentación de las api con OpenAPI(swagger)

--- src/main/java/com/ang/nav/config/AuditorConfig.java
+++ src/main/java/com/ang/nav/config/AuditorConfig.java
@@ -1,5 +1,7 @@
 package com.ang.nav.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -14,5 +16,14 @@ public class AuditorConfig {
     @Bean
     public AuditorAware<String> auditorProvider(){
         return () -> Optional.of("ANGEL_NAVARRO");
+    }
+
+    @Bean
+    public OpenAPI customOpenAPIO(){
+        return new OpenAPI()
+                .info(new Info()
+                .title("API CLIENTES")
+                .version("1.0")
+                .description("Documentación del API CLIENTES"));
     }
 }
